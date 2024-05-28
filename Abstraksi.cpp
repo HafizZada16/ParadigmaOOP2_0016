66eb63634e53d48fc19867618ad26d7e61a4858e
HafizZada16 <hafizzada16@gmail.com>
2024-05-28 18:56:10 +0700

    Add AbstraksiClass modifier public

--- Abstraksi.cpp
+++ Abstraksi.cpp
@@ -4,4 +4,16 @@ using namespace std;
 class AbstraksiClass {
 private: string x, y;
 
+public:
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "x = " << y << endl;
+    }
 };
