60e90e6c3d56bf24f65c71da9998adb266151bb8
HafizZada16 <hafizzada16@gmail.com>
2024-05-28 18:44:24 +0700

    Add class derivedClass

--- overriding.cpp
+++ overriding.cpp
@@ -4,6 +4,13 @@ using namespace std;
 class baseClass {
 public:
     virtual void perkenalan() {
-        cout << "Hallo saya Functioncdari base class"
+        cout << "Hallo saya Functioncdari base class";
+    }
+};
+
+class derivedClass : public baseClass {
+public:
+    void perkenalan() {
+        cout << "Hallo saya Functioncdari derived class";
     }
 };
